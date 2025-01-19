231e947c1b3dcff5639ccfef0aa080e9a51287da
weicanie <2042365244@qq.com>
2025-01-19 16:47:04 +0800

    更新github

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
 	//TODO 开启跨域
 	//TODO 前端大文件上传OSS
 	//TODO docker版
+	//TODO 整合chat后端
 	const app = await NestFactory.create(AppModule);
 	await app.listen(process.env.PORT ?? 3000);
 }
